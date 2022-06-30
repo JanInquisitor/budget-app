3f13bf46b596f8615bcc6f090604c979bc07a8ff
janinquisitor <jancarlos0195@gmail.com>
2022-06-30 19:38:09 -0400

    added the transfer method with its test

--- budget.py
+++ budget.py
@@ -1,3 +1,4 @@
+from re import M
 from typing import List, Any
 
 
@@ -7,21 +8,35 @@ class Category:
         self.ledger = ledger
 
     def deposit(self, amount: int, description: str = '') -> None:
+        # If the ledger attribute is of type `None` then it add an empty list to it.
         if self.ledger is None:
             self.ledger = []
-        self.ledger.append(
-            {'amount': amount, 'description': description}
-        )
+
+        # appends the new dictionary to the ledger list.
+        self.ledger.append({'amount': amount, 'description': description})
 
     def check_funds(self, amount: int) -> bool:
+        # Creates a funs variable
         funds: int = 0
+
+        # sum all of the deposits and withdraws amounts
         for i in range(len(self.ledger)):
             funds += self.ledger[i]['amount']
+
+        # If the passed amount is less or equal than the current funds then it returns `True`
         if amount <= funds:
+
+            # Returns true after confirming the condition
             return True
+
+        # Default value
         return False
 
     def get_balance(self) -> None:
+        # If the ledger attribute is of type `None` then it add an empty list to it.
+        if self.ledger is None:
+            self.ledger = []
+
         current_balance: int = 0
         for i in range(len(self.ledger)):
             current_balance += self.ledger[i]['amount']
@@ -30,21 +45,33 @@ class Category:
     def withdraw(self, amount: int, description: str = '') -> bool:
         # This checks if the passed ammount is greater than the overall available budget.
         if self.check_funds(amount):
+
             # This calls the `deposit` method with the amount passed converted to a negative integer with the description passed
             self.deposit(-abs(amount), description)
+
             # Returns `True` after the withdrawn amount is added to the ledger with the `deposit method`.
             return True
+
         # Default value
         return False
 
-    def transfer(self, amount: int, category: str) -> bool:
-      # A transfer method that accepts an amount and another budget category as arguments.
-      # The method should add a withdrawal with the amount and the description "Transfer to [Destination Budget Category]".
-      # The method should then add a deposit to the other budget category with the amount and the description "Transfer from [Source Budget Category]".
-      # If there are not enough funds, nothing should be added to either ledgers. This method should return True if the transfer took place, and False otherwise.
-        pass
+    def transfer(self, amount: int, target) -> bool:
+        # If the withdrawal is successfull then runs the target `deposit` function
+        if self.withdraw(amount, f'Transfer to {target.name.capitalize()}'):
+
+            # Adds the amount passed to the target object with message
+            target.deposit(amount, f'Transfer from {self.name.capitalize()}')
+
+            # Returns `True` if successfull
+            return True
+
+        # Default value
+        return False
 
     def __repr__(self):
+        char_limit: int  # a char limit variable
+        title: str = ['*'] * char_limit
+
         pass
 
     def __str__(self):
@@ -62,10 +89,19 @@ def create_spend_chart(categories: List[Category]):
     pass
 
 
-food = Category('food')
-food.deposit(900, 'For food')
-food.withdraw(10, 'pizza')
+# food = Category('food')
+# food.deposit(900, 'For food')
+# food.withdraw(10, 'pizza')
 
-entertaiment = Category('entertainment')
-entertaiment.deposit(500, 'for movies and games')
-entertaiment.withdraw(20, 'movies')
+# entertainment = Category('entertainment')
+# entertainment.deposit(500, 'for movies and games')
+# entertainment.withdraw(20, 'movies')
+# entertainment.get_balance()
+
+# print(f"entertainment before: {entertainment.ledger}")
+# print(f"food before: {food.ledger} \n")
+
+# food.transfer(200, entertainment)
+
+# print(f"entertainment after: {entertainment.ledger}")
+# print(f"food after: {food.ledger}")
--- test_budget.py
+++ test_budget.py
@@ -7,12 +7,12 @@ import budget
 class CategoryTest(unittest.TestCase):
     # Inicializa la clase que se prueba para que se use en toda la clase de prueba.
     def setUp(self) -> None:
-        self.video_games = budget.Category("video_games")
+        self.entertainment = budget.Category("entertainment")
         self.food = budget.Category('food')
 
     def test_instantiation(self):
-        actual = type(self.video_games)
-        expected = type(self.video_games)
+        actual = type(self.entertainment)
+        expected = type(self.entertainment)
         self.assertEqual(actual, expected,
                          "Expected instance of the class 'Category'.")
 
@@ -54,7 +54,33 @@ class CategoryTest(unittest.TestCase):
         actual: int = self.food.get_balance()
         expected: int = 900
         self.assertEqual(
-            actual, expected, "Expected `get balance method` to returrn the correct balanced of the category")
+            actual, expected, "Expected `get balance` method to return the correct balance of the category.")
+
+    def test_transfer(self):
+        self.food.deposit(900, "deposit")
+        self.food.withdraw(45.67, "milk, cereal, eggs, bacon, bread")
+        transfer_amount = 20
+        food_balance_before = self.food.get_balance()
+        entertainment_balance_before = self.entertainment.get_balance()
+        good_transfer = self.food.transfer(transfer_amount, self.entertainment)
+        food_balance_after = self.food.get_balance()
+        entertainment_balance_after = self.entertainment.get_balance()
+        actual = self.food.ledger[2]
+        expected = {"amount": -transfer_amount,
+                    "description": "Transfer to Entertainment"}
+        self.assertEqual(
+            actual, expected, 'Expected `transfer` method to create a specific ledger item in food object.')
+        self.assertEqual(good_transfer, True,
+                         'Expected `transfer` method to return `True`.')
+        self.assertEqual(food_balance_before - food_balance_after, transfer_amount,
+                         'Expected `transfer` method to reduce balance in food object.')
+        self.assertEqual(entertainment_balance_after - entertainment_balance_before, transfer_amount,
+                         'Expected `transfer` method to increase balance in entertainment object.')
+        actual = self.entertainment.ledger[0]
+        expected = {"amount": transfer_amount,
+                    "description": "Transfer from Food"}
+        self.assertEqual(
+            actual, expected, 'Expected `transfer` method to create a specific ledger item in entertainment object.')
 
 
 if __name__ == "__main__":
